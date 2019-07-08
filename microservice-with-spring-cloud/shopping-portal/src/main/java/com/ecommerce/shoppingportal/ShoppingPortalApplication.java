fbb7d591543a42a6413cc9ba3d3da30234155e66
achen-temp <achen-temp@nbme.org>
2019-07-08 10:56:37 -0400

    commit microservice complete project with spring cloud

---
+++ microservice-with-spring-cloud/shopping-portal/src/main/java/com/ecommerce/shoppingportal/ShoppingPortalApplication.java
@@ -0,0 +1,15 @@
+package com.ecommerce.shoppingportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+public class ShoppingPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingPortalApplication.class, args);
+	}
+
+}
